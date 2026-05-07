d1efcc02290fc19f067b583a6d2df565667b108c
Rolan228123 <RolanVataev22@gmail.com>
2026-05-07 14:32:11 -0400

    Finished feature @sep19 - tested and completed

--- steps/CommonSteps.ts
+++ steps/CommonSteps.ts
@@ -7,8 +7,21 @@ import {
   reviewPaymentPage,
   page,
 } from "../globalPagesSetup";
+import {fa, faker} from "@faker-js/faker";
 
 
 Given('user is on the enrollment page', async function () {
     await startApplicationPage.login();
+});
+
+When("user fills in all the fields with valid information",async function () {
+  await startApplicationPage.firstNameInputBox.fill(faker.person.firstName());
+  await startApplicationPage.lastNameInputBox.fill(faker.person.lastName());
+  await startApplicationPage.emailInputBox.fill(faker.internet.email());
+  await startApplicationPage.phoneNumberInputBox.fill(faker.string.numeric(10));
+  await startApplicationPage.selectHowDidYouHearAboutUs("Google");
+});
+
+When("user clicks on the next button", async function () {
+  await startApplicationPage.nextButton.click();
 });
--- steps/getting-started/clickNextButtonStep1.ts
+++ steps/getting-started/clickNextButtonStep1.ts
@@ -1,4 +1,20 @@
 import { Given, Then, When } from "@cucumber/cucumber";
-import { expect} from "@playwright/test";
+import { expect } from "@playwright/test";
 import { startApplicationPage, page } from "../../globalPagesSetup";
 import { productInfo } from "../../utilities/qa-data-reader";
+import { fa, faker } from "@faker-js/faker";
+
+Then("user sees step circle on start application is blue", async function () {
+  await expect(startApplicationPage.startApplicationStepCircle).toHaveCSS("background-color","rgb(1, 201, 255)");
+});
+
+When("user fills in only the required fields with valid information",async function () {
+    await startApplicationPage.firstNameInputBox.fill(faker.person.firstName());
+    await startApplicationPage.lastNameInputBox.fill(faker.person.lastName());
+    await startApplicationPage.emailInputBox.fill(faker.internet.email());
+    await startApplicationPage.phoneNumberInputBox.fill(faker.string.numeric(10));
+  });
+
+  Then("user sees step circle on payment plan is blue", async function () {
+    await expect(startApplicationPage.paymentPlanStepCircle).toHaveCSS("background-color","rgb(1, 201, 255)");
+  });
